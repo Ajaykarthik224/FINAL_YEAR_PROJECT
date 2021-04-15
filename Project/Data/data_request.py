cfd209f942addef4f33b079437ddca1c3d1cf3ad
AgentT30 <cthekkunja304@gmail.com>
2021-04-15 22:30:01 +0530

    Secured API key and removed JSON and CSV files

--- Project/Data/data_request.py
+++ Project/Data/data_request.py
@@ -1,10 +1,11 @@
 import requests
 import json
+import config
 
 
 class data_request:
 
-    api_key = '579b464db66ec23bdd00000138ba8e153b6f4e7e7065009210338f2b'
+    api_key = config.api_key
     url = f'https://api.data.gov.in/resource/8e0bd482-4aba-4d99-9cb9-ff124f6f1c2f?api-key={api_key}&format=json&offset=0&limit=10000000'
 
     data = requests.get(url).json()
